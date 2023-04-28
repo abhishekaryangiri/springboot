6a67db9abba06cf7d523b6974833888f6d066f78
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-04-28 23:38:33 +0530

    sb

---
+++ 1Intro/src/test/java/com/abhi/demo/abhiapp/AbhiappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.demo.abhiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbhiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
