18085e250daabb7727c7b8a2b6d8e66292650f07
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-04-28 23:44:06 +0530

    sbt

--- 1Intro/src/com/abhi/demo/abhiapp/AbhiappApplication.java
+++ 1Intro/src/com/abhi/demo/abhiapp/AbhiappApplication.java
@@ -3,6 +3,8 @@ package com.abhi.demo.abhiapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//bootstrap the sbt app
+
 @SpringBootApplication
 public class AbhiappApplication {
 
